686f0f62bda4b866ee2e415b68568a6f17b053de
Sushma <sushma.hegde@gmail.com>
2020-06-06 15:41:28 +0530

    addign it in the right place

---
+++ nd9990-c3-microservices-exercises/project/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME= slhegde
+export POSTGRES_PASSWORD= Actuallymypassword
+export POSTGRES_HOST= udagramdevhegde
+export POSTGRES_DB= udagramslhegde
+export AWS_BUCKET= udagramslhegde
+export AWS_REGION=Asia Pacific Ap-south-1
+export AWS_PROFILE='default'
+export JWT_SECRET= 'hello'
+export URL=http://localhost:8100
